30b0910753525f2bacc13bc3427178ad14a4cff8
Elena <rapid-steel@mail.ru>
2020-08-30 15:36:31 +0800

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "no-unused-vars": process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-unused-vars": 'off',
     "no-case-declarations": "off",
     "no-useless-escape": "off",
     "no-cond-assign": "warn"
